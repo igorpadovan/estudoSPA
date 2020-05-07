9e0d69dc3e996dd8e3756e65f7c37c898556dd67
Igor Padovan da Silva <igorpadovan@gmail.com>
2020-05-06 20:48:30 -0400

    roteamento

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "@angular/router";
+import { HomeComponent } from './navegacao/home/home.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+
+export const rootRouterCongig: Router = [
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent},
+    { path: 'contato', component: ContatoComponent},
+    { path: 'sobre', component: SobreComponent}
+];
